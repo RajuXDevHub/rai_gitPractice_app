62d1cc205c1020ccea55e76ce650198d8093437e
RajuXDevHub <rajuchowdhury5315@gmail.com>
2026-09-17 15:59:22 +0530

    these all files are for git practice

---
+++ src/main/java/gitPractice/firstDemo.java
@@ -0,0 +1,11 @@
+package gitPractice;
+
+public class firstDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is for git Practice ");
+
+	}
+
+}
